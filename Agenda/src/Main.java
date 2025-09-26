7bc3d62cfa5a3b972c00226dcbf42f01be1aa8b0
huugoo310 <hugomartinez310707@gmail.com>
2025-09-26 10:05:58 +0200

    init

--- Agenda/src/Main.java
+++ Agenda/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hofsdfsdfsdhjkhjkhjkhj");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Añadir contacto");
+        System.out.println("Mostrar contacto");
+        System.out.println("Buscar contacto");
+        System.out.println("Salir");
+        System.out.println("Elija una opción");
+
+        String opciones = sc.next();
+        String opciones1 = opciones;
+        System.out.println("Has elegifo la opcion " + opciones1);
     }
 }
